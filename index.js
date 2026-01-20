a1bc842b9820b536c46139027f7b9f2a35240764
Erika Harvey <erikaharvey@ufl.edu>
2026-01-20 12:48:35 -0500

    created quiz in index.js

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Your code here
+// Global array containing quiz questions
+var questionsArr = [
+    {
+        question: "Harry Potter was born in Godric's Hollow.",
+        answer: true
+    },
+    {
+        question: "Hogwarts has five houses.",
+        answer: false
+    },
+    {
+        question: "Hermione Granger is known for her intelligence and love of books.",
+        answer: true
+    },
+    {
+        question: "Voldemort is Harry Potter's Godfather.",
+        answer: false
+    },
+    {
+        question: "The spell 'Expelliarmus' is used to disarm an opponent.",
+        answer: true
+    }
+];
+
+// runQuiz function
+function runQuiz() {
+    var correctCount = 0;
+
+    // Loop through each question
+    for (var i = 0; i < questionsArr.length; i++) {
+        var userAnswer = confirm(questionsArr[i].question);
+
+        // Compare user's answer to correct answer
+        if (userAnswer === questionsArr[i].answer) {
+            correctCount++;
+        }
+    }
+
+    // Calculate percentage score
+    var scorePercentage = Math.round(
+        (correctCount / questionsArr.length) * 100
+    );
+
+    // Display final score
+    alert("You scored " + scorePercentage + "% on the Harry Potter Quiz!");
+}
